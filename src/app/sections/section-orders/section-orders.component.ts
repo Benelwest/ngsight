74e5ef7390a465b8c1d0ad2e064ef696e61e7f99
ben <benell@bluewin.ch>
2019-11-17 11:22:03 +0100

    Order List

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
+import { Customer } from '../../shared/customer';
 
 @Component({
   selector: 'app-section-orders',
@@ -9,6 +11,18 @@ export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
 
+  orders: Order[] = [
+    {id: 1, customer:
+      {id: 1 , name: 'test33', state: 'CO', email: 'main@example.ch'},
+      total: 230, placed: new Date(2017, 12, 1), fulfilles: new Date(2017, 12, 3)},
+    {id: 2, customer:
+        {id: 2 , name: 'test22', state: 'CO', email: 'main@example.ch'},
+        total: 230, placed: new Date(2017, 12, 1), fulfilles: new Date(2017, 12, 3)},
+    {id: 3, customer:
+          {id: 2 , name: 'test22dd', state: 'CO', email: 'main@example.ch'},
+          total: 230, placed: new Date(2017, 12, 1), fulfilles: new Date(2017, 12, 3)}
+
+  ];
   ngOnInit() {
   }
 
